6c4cd5b0ac7d8bdb8de7c66c8b03f31bb6fa169e
EJ <ejnunya@openfile.host>
2020-12-24 15:23:28 -0800

    Various improvements. Better exception handling.

--- errors.py
+++ errors.py
@@ -33,3 +33,12 @@ class ytdlPremiere(Exception):
     
     def __str__(self):
         return f'{self.message} {self.err}\n'
+
+class ytdlTimeout(Exception):
+    def __init__(self,message="YT-DL Timeout!",err=""):
+        self.message = message
+        self.err = err
+        super().__init__(self.message)
+    
+    def __str__(self):
+        return f'{self.message} {self.err}\n'
