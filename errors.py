6db5d71232cab40fb42b1f8ede378c8c016d8d9f
EJ <ejnunya@openfile.host>
2020-12-24 10:35:32 -0800

    Improve downloader. All sites now supported.

--- errors.py
+++ errors.py
@@ -1,5 +1,5 @@
 class ytdlNoVideo(Exception):
-    def __init__(self,message="YT-DL ERROR!",err=""):
+    def __init__(self,message="YT-DL No video!",err=""):
         self.message = message
         self.err = err
         super().__init__(self.message)
@@ -8,7 +8,25 @@ class ytdlNoVideo(Exception):
         return f'{self.message} {self.err}\n'
 
 class ytdlCopyright(Exception):
-    def __init__(self,message="YT-DL ERROR!",err=""):
+    def __init__(self,message="YT-DL Copyright Protected!",err=""):
+        self.message = message
+        self.err = err
+        super().__init__(self.message)
+    
+    def __str__(self):
+        return f'{self.message} {self.err}\n'
+
+class ytdlUnsupported(Exception):
+    def __init__(self,message="YT-DL Unsupported Site!",err=""):
+        self.message = message
+        self.err = err
+        super().__init__(self.message)
+    
+    def __str__(self):
+        return f'{self.message} {self.err}\n'
+
+class ytdlPremiere(Exception):
+    def __init__(self,message="YT-DL Waiting for Premiere!",err=""):
         self.message = message
         self.err = err
         super().__init__(self.message)
